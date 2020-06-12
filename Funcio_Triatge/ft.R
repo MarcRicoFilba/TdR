bffa7590c576923c05a39495264af7a1ca1e57c2
rpc01 <rpratc@gmail.com>
2020-06-12 18:54:34 +0200

    added quantmod library for prices download

--- Funcio_Triatge/ft.R
+++ Funcio_Triatge/ft.R
@@ -1,6 +1,14 @@
-file <- read.csv("CABK.MC.csv")
-serie <- file[,c("Date","Adj.Close")]
-ret<-log(serie[2:nrow(serie),2])-log(serie[1:(nrow(serie)-1),2])
-ret
+library(quantmod)   # get stock prices; useful stock analysis functions
+symbols <- c("CABK.MC") 
+serie <- getSymbols(symbols, src = 'yahoo', 
+             from = "2019-01-01",
+             to = "2020-06-01",
+             auto.assign = TRUE, warnings = FALSE) 
+serie <-get(serie)
+#file <- read.csv("CABK.MC.csv")
+serie <- serie[,6]
+serie<-as.data.frame(serie)
+ret<-log(serie[2:nrow(serie),1])-log(serie[1:(nrow(serie)-1),1])
 mean(ret)
 sd(ret)
+hist(ret, breaks=50)
